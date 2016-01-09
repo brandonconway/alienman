2f3692b0682b36abb7f43c4dda32335859c3849e
Brandon Conway <brandoncowboy@gmail.com>
2016-01-09 13:59:32 -0800

    Changes title to Alien Man

--- MainMenu.js
+++ MainMenu.js
@@ -11,14 +11,14 @@ BasicGame.MainMenu.prototype = {
 
 	create: function () {
                 console.log(this);
-                this.style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+                this.style = { font: "bold 32px Arial", fill: "black", boundsAlignH: "center", boundsAlignV: "middle" };
 		this.music = this.add.audio('titleMusic');
 		this.music.loop = true;
-//		this.music.play();
+		this.music.play();
 
 		this.background = this.add.sprite(0, 0, 'menuBackground');
 		this.background.scale.setTo(4, 2);
-		this.text = this.add.text(this.game.width/2, 40, "Play Rad Runner 5000", this.style).anchor.set(0.5);
+		this.text = this.add.text(this.game.width/2, 40, "Play Alien Man", this.style).anchor.set(0.5);
 		this.playButton = this.add.button(this.game.width/2, 100, 'playButton', this.startGame, this, 'buttonOver', 'buttonOut', 'buttonOver').anchor.set(0.5);
 
 	},
