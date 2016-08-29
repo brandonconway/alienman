ccbf4e808d07a86cbac5beea412f1f3259b5a173
Brandon Conway <conwayb@pdx.edu>
2016-08-28 17:38:25 -0700

    Simplify init in Boot state..

--- Boot.js
+++ Boot.js
@@ -1,44 +1,19 @@
 var BasicGame = {};
 
-BasicGame.Boot = function (game) {
-};
-
+BasicGame.Boot = function (game) {};
 BasicGame.Boot.prototype = {
 
     init: function () {
-
-        //  Unless you specifically know your game needs to support multi-touch I would recommend setting this to 1
-        this.input.maxPointers = 1;
-
-            //  In this case we're saying "scale the game, no lower than 480x260 and no higher than 1024x768"
-            //this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        //  Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
-        if (this.game.device.desktop)
-        {
-            this.scale.setMinMax(960, 640, 960, 640);
-            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            //  If you have any desktop specific settings, they can go in here
-            this.scale.pageAlignHorizontally = true;
-        }
-        else
-        {
-            //  Same goes for mobile settings.
-            this.scale.forceLandscape = true;
-            this.scale.pageAlignHorizontally = true;
-        }
-
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     },
 
     preload: function () {
-
         //  Here we load the assets required for our preloader (in this case a background and a loading bar)
         this.load.image('preloaderBackground', 'assets/images/starsky.jpeg');
         this.load.image('preloaderBar', 'assets/images/preloader_bar.png');
-
     },
 
     create: function () {
-
         //  By this point the preloader assets have loaded to the cache, we've set the game settings
         //  So now let's start the real preloader going
         this.state.start('Preloader');
