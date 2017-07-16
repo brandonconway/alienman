6320e96d59b51e7598ac08de9fb9d0a6ba674b08
Brandon Conway <conwayb@pdx.edu>
2017-07-16 16:42:28 -0700

    Make sure menu re-scales appropriately
    - This should be refactored to use constants and be more
    responsive for various device sizes.

--- MainMenu.js
+++ MainMenu.js
@@ -16,7 +16,8 @@ BasicGame.MainMenu.prototype = {
 		    this.background = this.add.sprite(0, 0, 'menuBackground');
 		    this.text = this.add.text(this.game.width/2+150, 400, "Play Alien Man", this.style).anchor.set(0.5);
 		    this.playButton = this.add.button(this.game.width/2, 400, 'playButton', this.startGame, this).anchor.set(0.5);
-
+            this.game.scale.setGameSize(960, 640);
+            this.game.scaled = false;
 	  },
 
     startGame: function (pointer) {
